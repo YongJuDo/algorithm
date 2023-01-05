101e53018d0a88d347862561b794200bd118d9ac
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-01-05 15:48:44 +0900

    [Bronze V] Title: 사칙연산, Time: 68 ms, Memory: 29200 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,14 @@
+values = input()
+values = values.split()
+a = int(values[0])
+b = int(values[1])
+# 코드 작성: A+B 결과값 출력
+print(int(a + b))
+# 코드 작성: A-B 결과값 출력
+print(int(a - b))
+# 코드 작성: A*B 결과값 출력
+print(int(a * b))
+# 코드 작성: A/B 결과값 출력
+print(int(a / b))
+# 코드 작성: A%B 결과값 출력
+print(int(a % b))
