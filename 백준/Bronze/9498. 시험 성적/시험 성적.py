c669e11f30b6e69a7e3337c9447ff8f0e0db6b76
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-01-09 18:00:36 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+num = int(input())
+
+if num <= 100 and num >= 90:
+  print('A')
+elif num <= 89 and num >= 80:
+  print('B')
+elif num <= 79 and num >= 70:
+  print('C')
+elif num <= 69 and num >= 60:
+  print('D')
+else:
+  print('F')
+  
+  
