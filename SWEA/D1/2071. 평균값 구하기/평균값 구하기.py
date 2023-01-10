7d7c5b421094e4cd92d8dfefeba44efe713ac114
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-01-10 14:58:32 +0900

    [D1] Title: 평균값 구하기, Time: 157 ms, Memory: 56,640 KB -BaekjoonHub

--- SWEA/D1/2071. 평균값 구하기/평균값 구하기.py
+++ SWEA/D1/2071. 평균값 구하기/평균값 구하기.py
@@ -2,11 +2,6 @@ tc = int(input())
 
 for t in range(1, tc + 1):
     num = list(map(int, input().split()))
-    sum = 0
-    avg = 0
-
-    for i in num:
-        sum += i
-        avg = sum / 10
-    
-    print(f"#{t} {round(avg)}")
+    avg = sum(num) / len(num)
+   
+    print(f"#{t} {round(avg)}") # round함수 -> 반올림
