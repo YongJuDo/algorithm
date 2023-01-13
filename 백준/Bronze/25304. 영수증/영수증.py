43190b2a9958e2fed2ba9c31710ed02563135892
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-01-13 17:47:52 +0900

    [Bronze V] Title: 영수증, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.py
@@ -0,0 +1,13 @@
+X = int(input())
+N = int(input())
+
+sum = 0
+for i in range(N):
+    a, b = map(int, input().split())
+    sum += a * b
+
+if X == sum:
+    print("Yes")
+else:
+    print("No")
+
