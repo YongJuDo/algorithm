514f575c95a000ab8bf56b205e33f6c545730628
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-02-01 14:34:33 +0900

    [Bronze II] Title: 점수 집계, Time: 36 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9076. 점수 집계/점수 집계.py
+++ 백준/Bronze/9076. 점수 집계/점수 집계.py
@@ -4,10 +4,7 @@ for _ in range(T):
     score = list(map(int, input().split()))
     score.remove(max(score))
     score.remove(min(score))
-    total = 0
-    if max(score) >= min(score) + 4:
+    if max(score) - min(score) >= 4:
         print("KIN")
     else:
-        for i in score:
-            total += i
-        print(total)
+        print(sum(score))
