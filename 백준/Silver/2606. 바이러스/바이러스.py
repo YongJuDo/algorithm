ec72eaa468ab42bef8aad13e3f60bff009b521c5
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-02-06 15:34:00 +0900

    [Silver III] Title: 바이러스, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,23 @@
-n = int(input())
-m = int(input())
-graph = [[] for _ in range(n+1)]
-visit = [False] * (n+1)
+n = int(input()) # 정점 개수 (컴퓨터)
+m = int(input()) # 간선 개수 (네트워크)
+graph = [[] for _ in range(n + 1)]
+visited = [False] * (n + 1)
 
-for i in range(m):
+for _ in range(m):
     v1, v2 = map(int, input().split())
     graph[v1].append(v2)
     graph[v2].append(v1)
 
 def dfs(start):
     stack = [start]
-    visit[start] = True
+    visited[start] = True
 
     while stack:
         cur = stack.pop()
 
         for adj in graph[cur]:
-            if not visit[adj]:
-                visit[adj] = True
+            if not visited[adj]:
+                visited[adj] = True
                 stack.append(adj)
 dfs(1)
-print(sum(visit)-1)
+print(sum(visited) - 1)
