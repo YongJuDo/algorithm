1d77f5e50f58b60a571c17f69a703774e5f350c3
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-02-09 14:08:17 +0900

    [Silver IV] Title: 스택, Time: 48 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+stack = []
+
+for _ in range(N):
+    cmd = input().split()
+
+    if cmd[0] == 'push':
+        stack.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(stack))
+    elif cmd[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
