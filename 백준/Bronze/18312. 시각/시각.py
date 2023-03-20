19d9096beb55693c1278bad2f34ac8a0ecefea74
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-03-20 21:51:55 +0900

    [Bronze II] Title: 시각, Time: 100 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/18312. 시각/시각.py
@@ -0,0 +1,16 @@
+n, k  = map(int, input().split())
+cnt = 0
+
+for h in range(n+1):
+    if h < 10:
+        h = '0' + str(h)
+    for m in range(60):
+        if m < 10:
+            m = '0' + str(m)
+        for s in range(60):
+            if s < 10:
+                s = '0' + str(s)
+            if str(k) in str(h) + str(m) + str(s):
+                cnt += 1
+
+print(cnt)
