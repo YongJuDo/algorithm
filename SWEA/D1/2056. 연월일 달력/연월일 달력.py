5985dffcd9bcf82d3ccd3002dc31af161c44ef18
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-04-26 21:53:55 +0900

    [D1] Title: 연월일 달력, Time: 144 ms, Memory: 58,524 KB -BaekjoonHub

--- SWEA/D1/2056. 연월일 달력/연월일 달력.py
+++ SWEA/D1/2056. 연월일 달력/연월일 달력.py
@@ -1,16 +1,16 @@
 T = int(input())
-date_dic = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-result = ''
 
-for tc in range(1, T + 1):
-    date = input()
+for i in range(1, T+1):
+    date = str(input())
+    days = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
     year = date[:4]
     month = date[4:6]
     day = date[6:]
 
-    if (0 < int(month) < 13) and (int(day) <= date_dic[int(month)]):
-        result = year + '/' + month + '/' + day
+    slash = ''
+    if 0 < int(month) < 13 and int(day) <= days[int(month)]:
+        slash = year + '/' + month + '/' + day
     else:
-        result = -1
+        slash = '-1'
 
-    print(f'#{tc} {result}')
+    print('#' + str(i), slash)
