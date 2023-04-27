f6b42a7b00be4ad6af9fcfcd213f4e2b8e44be92
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-04-27 20:31:27 +0900

    [D2] Title: 간단한 소인수분해, Time: 160 ms, Memory: 58,280 KB -BaekjoonHub

--- SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
+++ SWEA/D2/1945. 간단한 소인수분해/간단한 소인수분해.py
@@ -1,24 +1,14 @@
 T = int(input())
 
-for i in range(1, T+1):
+for tc in range(1, T+1):
     num = int(input())
+    num_list = [2, 3, 5, 7, 11]
+    array = [0] * 5
+    
+    for i in range(len(num_list)):
+        while(num % num_list[i] == 0):
+            array[i] += 1
+            num //= num_list[i]
 
-    a, b, c, d, e = 0, 0, 0, 0, 0
-    while num != 1:
-        if num % 2 == 0:
-            a += 1
-            num = num // 2
-        elif num % 3 == 0:
-            b += 1
-            num = num // 3
-        elif num % 5 == 0:
-            c += 1
-            num = num // 5
-        elif num % 7 == 0:
-            d += 1
-            num = num // 7
-        elif num % 11 == 0:
-            e += 1
-            num = num // 11
-
-    print('#'+str(i), a, b, c, d, e)
+    print('#{} '.format(tc), end = '')
+    print(*array)
