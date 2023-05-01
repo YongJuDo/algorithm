c2a7eef6e3df361a2eb273eac1aa84dd0f4465fc
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-05-01 21:40:23 +0900

    [D1] Title: 1대1 가위바위보, Time: 142 ms, Memory: 58,256 KB -BaekjoonHub

---
+++ SWEA/D1/1936. 1대1 가위바위보/1대1 가위바위보.py
@@ -0,0 +1,14 @@
+A, B = map(int, input().split())
+
+if A == 1 and B == 3:
+    print('A')
+if A == 2 and B == 1:
+    print('A')
+if A == 3 and B == 2:
+    print('A')
+if B == 1 and A == 3:
+    print('B')
+if B == 2 and A == 1:
+    print('B')
+if B == 3 and A == 2:
+    print('B')
