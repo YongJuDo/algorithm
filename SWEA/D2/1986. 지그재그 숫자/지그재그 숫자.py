46043a17d14a75a9ed4c22b5fe242fbc79d933e3
YongJuDo <82207310+YongJuDo@users.noreply.github.com>
2023-05-13 23:13:53 +0900

    [D2] Title: 지그재그 숫자, Time: 147 ms, Memory: 58,528 KB -BaekjoonHub

---
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
@@ -0,0 +1,12 @@
+T = int(input())
+
+for i in range(1, T + 1):
+    N = int(input())
+    sum = 0
+    for j in range(1, N + 1):
+        if j % 2 != 0:
+            sum += j
+        else:
+            sum -= j
+
+    print('#' + str(i), sum)
